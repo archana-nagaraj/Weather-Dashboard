8e11c2ed00a3326310504f5498c9df983d9dd6ef
Archana Nagaraja <johndoe@example.com>
2021-01-10 09:17:26 -0800

    added function for fetching 5-Day weather forecase by cityname

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@
 //  Global Variables
 const apiKey = "3b252b3f0afd61300c13bbf7516fcbb5";
 
-var getWeatherData = function(cityName){
+var getweatherData = function(cityName){
     // format the weather dashboard api url
     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" +cityName + "&appid=" +apiKey;
     // make a request to the url
@@ -22,7 +22,24 @@ var getWeatherData = function(cityName){
     });
 };
 
-getWeatherData("London");
-getWeatherData("Bangalore");
-getWeatherData("San Jose");
+getweatherData("London");
+// getweatherData("Bangalore");
+// getweatherData("San Jose");
 
+
+// fetch 5-Day Weather Forecast by city
+var getweatherData_5DayForecast = function(cityName){
+    // format the weather dashboard api url
+   var apiUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" +cityName + "&cnt=5&appid=" +apiKey;
+    // make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    
+    });
+};
+
+getweatherData_5DayForecast("London");
+// getweatherData_5DayForecast("Bangalore");
+// getweatherData_5DayForecast("San Jose");
